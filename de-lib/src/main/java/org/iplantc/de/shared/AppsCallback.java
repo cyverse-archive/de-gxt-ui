0e874f8a2f3a1bed5ee39c72d71d86a0e9fb5afc
Ashley Ramsey <aramsey@cyverse.org>
2016-12-01 11:01:15 -0700

    CORE-8367 Update AppsCallback to manage Apps, App Editor, App Launch, and Workflow windows

--- de-lib/src/main/java/org/iplantc/de/shared/AppsCallback.java
+++ de-lib/src/main/java/org/iplantc/de/shared/AppsCallback.java
@@ -2,13 +2,20 @@ package org.iplantc.de.shared;
 
 import org.iplantc.de.client.models.WindowType;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 /**
  * @author aramsey
  */
 public abstract class AppsCallback<T> implements DECallback<T> {
 
     @Override
-    public WindowType getWindowType() {
-        return WindowType.APPS;
+    public List<WindowType> getWindowTypes() {
+        return Lists.newArrayList(WindowType.APPS,
+                                  WindowType.APP_INTEGRATION,
+                                  WindowType.APP_WIZARD,
+                                  WindowType.WORKFLOW_INTEGRATION);
     }
 }
