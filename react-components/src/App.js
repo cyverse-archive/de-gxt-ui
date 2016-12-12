36b3391905ca080e6613aed1bd95acc985367002
Paul Sarando <psarando@cyverse.org>
2016-12-12 16:41:53 -0700

    Initial clipboardFunctions app.

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {hasClipboardAPI, copySelection} from './clipboardFunctions'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        // This binding is necessary to make `this` work in the callback
+        this.onCopyText = this.onCopyText.bind(this);
+    }
+
+    renderCopyTextArea(text) {
+        return <textArea ref="copyTextArea" value={text} readOnly="readonly" />;
+    }
+
+    renderCopyButton() {
+        if (hasClipboardAPI()) {
+            return <button ref="copyTextBtn" type="button" onClick={this.onCopyText}>Copy</button>;
+        }
+    }
+
+    onCopyText(e) {
+        e.preventDefault();
+
+        this.refs.copyTextArea.select();
+        if (copySelection()) {
+            this.refs.copyTextBtn.innerText = "Copied!";
+        }
+    }
+
+    componentDidMount() {
+        if (!hasClipboardAPI()) {
+            this.refs.copyTextArea.select();
+        }
+    }
+
+    render() {
+        let copyTextArea = this.renderCopyTextArea(this.props.text),
+            copyButton = this.renderCopyButton();
+
+        return (
+            <div className="App">
+                {copyTextArea}
+                {copyButton}
+            </div>
+        );
+    }
 }
 
 export default App;
--- react-components/src/index.js
+++ react-components/src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App text={
+`The Dark Arts better be worried,
+oh boy!`
+    }/>,
+    document.getElementById('root')
+);
 registerServiceWorker();
