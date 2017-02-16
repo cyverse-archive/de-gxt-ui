6c6779a8c59d718c58a9492fbf790f2ce11ebabb
Ashley Ramsey <aramsey@iplantcollaborative.org>
2017-02-16 00:14:57 -0700

    Add gwt-react to project

--- react-components/src/lib.js
+++ react-components/src/lib.js
@@ -1,3 +1,10 @@
 import * as apps from './apps';
 
+import React from "react";
+import ReactDOM from "react-dom";
+
+// gwt-react needs React and ReactDOM on the global object
+window.React = React;
+window.ReactDOM = ReactDOM;
+
 export { apps };
