60269120741b28aff127e94f9ab6cc4eb5489aaa
Ashley Ramsey <aramsey@iplantcollaborative.org>
2017-02-21 23:03:48 -0700

    CORE-8602 Add events for AnalysisToolbarImpl

---
+++ de-lib/src/main/java/org/iplantc/de/analysis/client/events/AnalysisCommentUpdate.java
@@ -0,0 +1,48 @@
+package org.iplantc.de.analysis.client.events;
+
+import org.iplantc.de.client.models.analysis.Analysis;
+
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.shared.HandlerRegistration;
+
+/**
+ * @author aramsey
+ */
+public class AnalysisCommentUpdate extends GwtEvent<AnalysisCommentUpdate.AnalysisCommentUpdateHandler> {
+
+    public static interface AnalysisCommentUpdateHandler extends EventHandler {
+        void onAnalysisCommentUpdate(AnalysisCommentUpdate event);
+    }
+
+    public interface HasAnalysisCommentUpdateHandlers {
+        HandlerRegistration addAnalysisCommentUpdateHandler(AnalysisCommentUpdateHandler handler);
+    }
+
+    private Analysis analysis;
+    private String comment;
+
+    public AnalysisCommentUpdate(Analysis analysis, String comment) {
+
+        this.analysis = analysis;
+        this.comment = comment;
+    }
+
+    public static Type<AnalysisCommentUpdateHandler> TYPE = new Type<AnalysisCommentUpdateHandler>();
+
+    public Type<AnalysisCommentUpdateHandler> getAssociatedType() {
+        return TYPE;
+    }
+
+    protected void dispatch(AnalysisCommentUpdateHandler handler) {
+        handler.onAnalysisCommentUpdate(this);
+    }
+
+    public Analysis getAnalysis() {
+        return analysis;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+}
