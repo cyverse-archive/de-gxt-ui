a4f11061ac079b5e6c2680c05d22df3918adc4bf
Ashley Ramsey <aramsey@cyverse.org>
2017-03-16 13:28:51 -0700

    CORE-8643 Set up PropertyEditorAppearance

---
+++ de-lib/src/main/java/org/iplantc/de/apps/integration/client/view/propertyEditors/PropertyEditorAppearance.java
@@ -0,0 +1,7 @@
+package org.iplantc.de.apps.integration.client.view.propertyEditors;
+
+/**
+ * @author aramsey
+ */
+public interface PropertyEditorAppearance {
+}
---
+++ de-lib/src/main/java/org/iplantc/de/theme/base/client/apps/integration/propertyEditors/PropertyEditorDefaultAppearance.java
@@ -0,0 +1,9 @@
+package org.iplantc.de.theme.base.client.apps.integration.propertyEditors;
+
+import org.iplantc.de.apps.integration.client.view.propertyEditors.PropertyEditorAppearance;
+
+/**
+ * @author aramsey
+ */
+public class PropertyEditorDefaultAppearance implements PropertyEditorAppearance {
+}
