2df87c446b0af98003c21fa741316db171ce3277
Ashley Ramsey <aramsey@cyverse.org>
2017-03-16 14:41:02 -0700

    CORE-8643 Switch AddValidatorDialog to use PropertyEditorAppearance

--- de-lib/src/main/java/org/iplantc/de/apps/integration/client/view/propertyEditors/PropertyEditorAppearance.java
+++ de-lib/src/main/java/org/iplantc/de/apps/integration/client/view/propertyEditors/PropertyEditorAppearance.java
@@ -16,4 +16,56 @@ public interface PropertyEditorAppearance {
     String groupNameEmptyText();
 
     String groupDelete();
+
+    String regex(String regex);
+
+    String characterLimit(int limit);
+
+    String intAbove(int above);
+
+    String intBelow(int below);
+
+    String intRange(int above, int below);
+
+    String dblAbove(double above);
+
+    String dblBelow(double below);
+
+    String dblRange(double above, double below);
+
+    String ruleType();
+
+    String valueRegex();
+
+    String valueCharLimit();
+
+    String valueAbove();
+
+    String valueBelow();
+
+    String valueBetween();
+
+    String valueBetweenAnd();
+
+    String validatorDialogHeading();
+
+    String regexLabel();
+
+    String characterLimitLabel();
+
+    String intAboveLabel();
+
+    String intBelowLabel();
+
+    String intRangeLabel();
+
+    String dblAboveLabel();
+
+    String dblBelowLabel();
+
+    String dblRangeLabel();
+
+    String validatorDialogWidth();
+
+    String validatorDialogHeight();
 }
--- de-lib/src/main/java/org/iplantc/de/theme/base/client/apps/integration/propertyEditors/PropertyEditorDefaultAppearance.java
+++ de-lib/src/main/java/org/iplantc/de/theme/base/client/apps/integration/propertyEditors/PropertyEditorDefaultAppearance.java
@@ -3,6 +3,7 @@ package org.iplantc.de.theme.base.client.apps.integration.propertyEditors;
 import org.iplantc.de.apps.integration.client.view.AppsEditorPanelAppearance;
 import org.iplantc.de.apps.integration.client.view.propertyEditors.PropertyEditorAppearance;
 import org.iplantc.de.resources.client.uiapps.widgets.AppsWidgetsPropertyPanelLabels;
+import org.iplantc.de.resources.client.uiapps.widgets.ArgumentValidatorMessages;
 
 import com.google.gwt.core.client.GWT;
 
@@ -13,16 +14,20 @@ public class PropertyEditorDefaultAppearance implements PropertyEditorAppearance
 
     private AppsWidgetsPropertyPanelLabels propertyPanelLabels;
     private AppsEditorPanelAppearance panelAppearance;
+    private ArgumentValidatorMessages argumentValidatorMessages;
 
     public PropertyEditorDefaultAppearance() {
         this ((AppsWidgetsPropertyPanelLabels)GWT.create(AppsWidgetsPropertyPanelLabels.class),
-              (AppsEditorPanelAppearance)GWT.create(AppsEditorPanelAppearance.class));
+              (AppsEditorPanelAppearance)GWT.create(AppsEditorPanelAppearance.class),
+              (ArgumentValidatorMessages)GWT.create(ArgumentValidatorMessages.class));
     }
 
     public PropertyEditorDefaultAppearance(AppsWidgetsPropertyPanelLabels propertyPanelLabels,
-                                           AppsEditorPanelAppearance panelAppearance) {
+                                           AppsEditorPanelAppearance panelAppearance,
+                                           ArgumentValidatorMessages argumentValidatorMessages) {
         this.propertyPanelLabels = propertyPanelLabels;
         this.panelAppearance = panelAppearance;
+        this.argumentValidatorMessages = argumentValidatorMessages;
     }
 
     @Override
@@ -48,5 +53,135 @@ public class PropertyEditorDefaultAppearance implements PropertyEditorAppearance
     @Override
     public String groupDelete() {
         return propertyPanelLabels.groupDelete();
+    }
+
+    @Override
+    public String regex(String regex) {
+        return argumentValidatorMessages.regex(regex);
+    }
+
+    @Override
+    public String characterLimit(int limit) {
+        return argumentValidatorMessages.characterLimit(limit);
+    }
+
+    @Override
+    public String intAbove(int above) {
+        return argumentValidatorMessages.intAbove(above);
+    }
+
+    @Override
+    public String intBelow(int below) {
+        return argumentValidatorMessages.intBelow(below);
+    }
+
+    @Override
+    public String intRange(int above, int below) {
+        return argumentValidatorMessages.intRange(above, below);
+    }
+
+    @Override
+    public String dblAbove(double above) {
+        return argumentValidatorMessages.dblAbove(above);
+    }
+
+    @Override
+    public String dblBelow(double below) {
+        return argumentValidatorMessages.dblBelow(below);
+    }
+
+    @Override
+    public String dblRange(double above, double below) {
+        return argumentValidatorMessages.dblRange(above, below);
+    }
+
+    @Override
+    public String ruleType() {
+        return argumentValidatorMessages.ruleType();
+    }
+
+    @Override
+    public String valueRegex() {
+        return argumentValidatorMessages.valueRegex();
+    }
+
+    @Override
+    public String valueCharLimit() {
+        return argumentValidatorMessages.valueCharLimit();
+    }
+
+    @Override
+    public String valueAbove() {
+        return argumentValidatorMessages.valueAbove();
+    }
+
+    @Override
+    public String valueBelow() {
+        return argumentValidatorMessages.valueBelow();
+    }
+
+    @Override
+    public String valueBetween() {
+        return argumentValidatorMessages.valueBetween();
+    }
+
+    @Override
+    public String valueBetweenAnd() {
+        return argumentValidatorMessages.valueBetweenAnd();
+    }
+
+    @Override
+    public String validatorDialogHeading() {
+        return argumentValidatorMessages.validatorDialogHeading();
+    }
+
+    @Override
+    public String regexLabel() {
+        return argumentValidatorMessages.regexLabel();
+    }
+
+    @Override
+    public String characterLimitLabel() {
+        return argumentValidatorMessages.characterLimitLabel();
+    }
+
+    @Override
+    public String intAboveLabel() {
+        return argumentValidatorMessages.intAboveLabel();
+    }
+
+    @Override
+    public String intBelowLabel() {
+        return argumentValidatorMessages.intBelowLabel();
+    }
+
+    @Override
+    public String intRangeLabel() {
+        return argumentValidatorMessages.intRangeLabel();
+    }
+
+    @Override
+    public String dblAboveLabel() {
+        return argumentValidatorMessages.dblAboveLabel();
+    }
+
+    @Override
+    public String dblBelowLabel() {
+        return argumentValidatorMessages.dblBelowLabel();
+    }
+
+    @Override
+    public String dblRangeLabel() {
+        return argumentValidatorMessages.dblRangeLabel();
+    }
+
+    @Override
+    public String validatorDialogWidth() {
+        return "400";
+    }
+
+    @Override
+    public String validatorDialogHeight() {
+        return "250";
     }
 }
