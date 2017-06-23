2009b2dd2840f00ceabe14f5c89b8ac7c100d0ca
Sriram Srinivasan <sriram@cyverse.org>
2017-06-23 16:30:46 -0700

    Refactor code to use Environment

--- de-webapp/src/main/java/org/iplantc/de/server/HttpSessionConfig.java
+++ de-webapp/src/main/java/org/iplantc/de/server/HttpSessionConfig.java
@@ -1,27 +1,22 @@
 package org.iplantc.de.server;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 /**
  * Created by sriram on 6/16/17.
  */
 
 @EnableRedisHttpSession
 @Configuration
-@PropertySource("file:/etc/iplant/de/de.properties")
 public class HttpSessionConfig {
 
-
-    @Value("${redis.host}")
-    private String redisHostName;
-
-    @Value("${redis.port}")
-    private int redisPort;
+    @Autowired
+    private Environment environment;
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
@@ -31,10 +26,18 @@ public class HttpSessionConfig {
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
         JedisConnectionFactory factory = new JedisConnectionFactory();
-        factory.setHostName(redisHostName);
-        factory.setPort(redisPort);
+        factory.setHostName(getHostName());
+        factory.setPort(getPort());
         factory.setUsePool(true);
         return factory;
+    }
+
+    private String getHostName() {
+        return environment.getProperty("redis.host");
+    }
+
+    private int getPort() {
+        return Integer.parseInt(environment.getProperty("redis.port"));
     }
 
 }
