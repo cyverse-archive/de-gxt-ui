1637680038d41785d0bcf93cb0c89f941e1060b9
Sriram Srinivasan <sriram@cyverse.org>
2017-06-26 13:16:14 -0700

    core-8932 Read configuration for Redis from property file.

--- de-webapp/src/main/java/org/iplantc/de/server/HttpSessionConfig.java
+++ de-webapp/src/main/java/org/iplantc/de/server/HttpSessionConfig.java
@@ -1,5 +1,7 @@
 package org.iplantc.de.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +9,7 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
 /**
  * Created by sriram on 6/16/17.
  */
@@ -18,6 +21,9 @@ public class HttpSessionConfig {
     @Autowired
     private Environment environment;
 
+    private final Logger LOG = LoggerFactory.getLogger(HttpSessionConfig.class);
+
+
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
@@ -28,16 +34,28 @@ public class HttpSessionConfig {
         JedisConnectionFactory factory = new JedisConnectionFactory();
         factory.setHostName(getHostName());
         factory.setPort(getPort());
+        factory.setDatabase(getDbNumber());
         factory.setUsePool(true);
+        factory.setPassword(getPassword());
         return factory;
     }
 
     private String getHostName() {
-        return environment.getProperty("redis.host");
+        return environment.getProperty("org.iplantc.discoveryenvironment.redis.host");
     }
 
     private int getPort() {
-        return Integer.parseInt(environment.getProperty("redis.port"));
+        return Integer.parseInt(environment.getProperty("org.iplantc.discoveryenvironment.redis.port"));
+    }
+
+    private int getDbNumber() {
+        return Integer.parseInt(environment.getProperty(
+                "org.iplantc.discoveryenvironment.redis.db.number"));
+    }
+
+    private String getPassword() {
+        return environment.getProperty("org.iplantc.discoveryenvironement.redis.password");
     }
 
 }
+
