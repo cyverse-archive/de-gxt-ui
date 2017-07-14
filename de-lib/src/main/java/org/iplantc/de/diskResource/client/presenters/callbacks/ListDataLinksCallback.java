ba3368e781ca923900cd4d25e8042404c3e042ad
Sriram Srinivasan <sriram@cyverse.org>
2017-07-14 09:25:47 -0700

    core-8648 remove commented code.

--- de-lib/src/main/java/org/iplantc/de/diskResource/client/presenters/callbacks/ListDataLinksCallback.java
+++ de-lib/src/main/java/org/iplantc/de/diskResource/client/presenters/callbacks/ListDataLinksCallback.java
@@ -28,35 +28,6 @@ public class ListDataLinksCallback<M> extends DataCallback<FastMap<List<DataLink
     @Override
     public void onSuccess(FastMap<List<DataLink>> result) {
         // Get tickets by resource id, add them to the tree.
-/*       JSONObject response = jsonUtil.getObject(result);
-        JSONObject tickets = jsonUtil.getObject(response, "tickets");
-
-        Splittable placeHolder;
-        for (String key : tickets.keySet()) {
-            placeHolder = StringQuoter.createSplittable();
-            M dr = null;
-            // manually find the item since id's wont work
-            for (M item : tree.getStore().getAll()) {
-                if (((DiskResource)item).getPath().equals(key)) {
-                    dr = item;
-                    break;
-                }
-            }
-
-            JSONArray dlIds = jsonUtil.getArray(tickets, key);
-            Splittable splittable = StringQuoter.split(dlIds.toString());
-            splittable.assign(placeHolder, "tickets");
-            AutoBean<DataLinkList> ticketsAB = AutoBeanCodex.decode(dlFactory,
-                                                                    DataLinkList.class,
-                                                                    placeHolder);
-
-            List<DataLink> dlList = ticketsAB.as().getTickets();
-
-            for (DataLink dl : dlList) {
-                tree.getStore().add(dr, (M)dl);
-                tree.setChecked((M)dl, CheckState.CHECKED);
-            }
-        }*/
         // Select all roots automatically
         result.keySet().forEach(key -> {
             M dr = null;
