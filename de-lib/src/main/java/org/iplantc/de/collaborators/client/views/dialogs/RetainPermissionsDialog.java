4e89a810ce2ffe4f0903b6646d995524027cc804
Ashley Ramsey <aramsey@iplantcollaborative.org>
2017-07-25 10:16:30 -0700

    CORE-8941 Add static IDs to retain perms prompt

--- de-lib/src/main/java/org/iplantc/de/collaborators/client/views/dialogs/RetainPermissionsDialog.java
+++ de-lib/src/main/java/org/iplantc/de/collaborators/client/views/dialogs/RetainPermissionsDialog.java
@@ -1,6 +1,7 @@
 package org.iplantc.de.collaborators.client.views.dialogs;
 
 import org.iplantc.de.collaborators.client.ManageCollaboratorsView;
+import org.iplantc.de.collaborators.shared.CollaboratorsModule;
 
 import com.google.inject.Inject;
 
@@ -19,5 +20,16 @@ public class RetainPermissionsDialog extends ConfirmMessageBox {
         setPredefinedButtons(PredefinedButton.YES, PredefinedButton.NO, PredefinedButton.CANCEL);
         getButton(PredefinedButton.YES).setText(appearance.retainPermissionsBtn());
         getButton(PredefinedButton.NO).setText(appearance.removePermissionsBtn());
+
+        ensureDebugId(CollaboratorsModule.Ids.RETAIN_PERMS_DLG);
+    }
+
+    @Override
+    protected void onEnsureDebugId(String baseID) {
+        super.onEnsureDebugId(baseID);
+
+        getButton(PredefinedButton.YES).ensureDebugId(baseID + CollaboratorsModule.Ids.RETAIN_PERMS_BTN);
+        getButton(PredefinedButton.NO).ensureDebugId(baseID + CollaboratorsModule.Ids.REMOVE_PERMS_BTN);
+        getButton(PredefinedButton.CANCEL).ensureDebugId(baseID + CollaboratorsModule.Ids.CANCEL_BTN);
     }
 }
