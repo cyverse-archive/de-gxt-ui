504fbbf2b0ae85fa92bc0931307525fb3c340bc7
Ashley Ramsey <aramsey@iplantcollaborative.org>
2017-08-29 17:44:22 -0700

    CORE-8990 Add tests

--- de-lib/src/main/java/org/iplantc/de/collaborators/client/util/UserSearchField.java
+++ de-lib/src/main/java/org/iplantc/de/collaborators/client/util/UserSearchField.java
@@ -46,7 +46,7 @@ import java.util.Comparator;
 public class UserSearchField implements IsWidget,
                                         UserSearchResultSelected.HasUserSearchResultSelectedEventHandlers {
 
-    class SubjectSortComparator implements Comparator<Subject> {
+    public static class SubjectSortComparator implements Comparator<Subject> {
 
         @Override
         public int compare(Subject o1, Subject o2) {
