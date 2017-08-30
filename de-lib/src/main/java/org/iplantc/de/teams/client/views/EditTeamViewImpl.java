4ce898242ece3e7cf7ff337715f973a1ea4e3be2
Ashley Ramsey <aramsey@iplantcollaborative.org>
2017-08-30 09:57:53 -0700

    CORE-8978 Fix null team name issue

--- de-lib/src/main/java/org/iplantc/de/teams/client/views/EditTeamViewImpl.java
+++ de-lib/src/main/java/org/iplantc/de/teams/client/views/EditTeamViewImpl.java
@@ -189,7 +189,7 @@ public class EditTeamViewImpl extends Composite implements EditTeamView,
 
     public void edit(Group group) {
         editorDriver.edit(group);
-        nameEditor.setValue(group.getSubjectDisplayName());
+        nameEditor.setValue(group.getGroupShortName());
     }
 
     @Override
