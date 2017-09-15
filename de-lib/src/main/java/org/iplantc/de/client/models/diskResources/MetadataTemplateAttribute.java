20b636602c0044accf86d6552b1ecfc196ce476a
Paul Sarando <psarando@cyverse.org>
2017-09-15 16:19:58 -0700

    CORE-8982 Add MetadataTemplateAttribute JSON settings.
    For use in OLS attributes as query param settings, but also for future
    use in Unified Astronomy Thesaurus (UAT) param settings
    or other UI-only settings.

--- de-lib/src/main/java/org/iplantc/de/client/models/diskResources/MetadataTemplateAttribute.java
+++ de-lib/src/main/java/org/iplantc/de/client/models/diskResources/MetadataTemplateAttribute.java
@@ -4,15 +4,15 @@ import org.iplantc.de.client.models.HasDescription;
 import org.iplantc.de.client.models.HasId;
 
 import com.google.gwt.user.client.ui.HasName;
+import com.google.web.bindery.autobean.shared.Splittable;
 
 import java.util.List;
 
-public interface MetadataTemplateAttribute extends HasId, HasName,
-		HasDescription {
+public interface MetadataTemplateAttribute extends HasId, HasName, HasDescription {
 
-	String getType();
+    String getType();
 
-	void setType(String type);
+    void setType(String type);
 
     void setRequired(Boolean required);
 
@@ -24,4 +24,19 @@ public interface MetadataTemplateAttribute extends HasId, HasName,
     // Applicable only for Enum type
     void setValues(List<TemplateAttributeSelectionItem> values);
 
+    /**
+     * Arbitrary JSON settings, set by Belphegor admins for use in the DE Metadata Template view.
+     * For example, as OLS query param settings for metadata term attributes.
+     *
+     * @return Arbitrary JSON settings.
+     */
+    Splittable getSettings();
+
+    /**
+     * Arbitrary JSON settings, set by Belphegor admins for use in the DE Metadata Template view.
+     * For example, as OLS query param settings for metadata term attributes.
+     *
+     * @param settings Arbitrary JSON settings.
+     */
+    void setSettings(Splittable settings);
 }
