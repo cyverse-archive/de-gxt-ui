027fc88b3b7aaf379e460043cb660c33de096096
Ashley Ramsey <aramsey@iplantcollaborative.org>
2017-09-18 10:59:23 -0700

    CORE-8644 Add missing editors to ArgumentEditorGinFactory

--- de-lib/src/main/java/org/iplantc/de/apps/widgets/client/gin/factory/ArgumentEditorGinFactory.java
+++ de-lib/src/main/java/org/iplantc/de/apps/widgets/client/gin/factory/ArgumentEditorGinFactory.java
@@ -1,9 +1,26 @@
 package org.iplantc.de.apps.widgets.client.gin.factory;
 
 import org.iplantc.de.apps.widgets.client.view.editors.FileFolderInputEditor;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.DoubleInputEditor;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.DoubleSelectionEditor;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.EnvironmentVariableEditor;
 import org.iplantc.de.apps.widgets.client.view.editors.arguments.FileInputEditor;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.FileOutputEditor;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.FlagEditor;
 import org.iplantc.de.apps.widgets.client.view.editors.arguments.FolderInputEditor;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.FolderOutputEditor;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.InfoEditor;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.IntegerInputEditor;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.IntegerSelectionEditor;
 import org.iplantc.de.apps.widgets.client.view.editors.arguments.MultiFileInputEditor;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.MultiFileOutputEditor;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.MultiLineTextEditor;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.ReferenceAnnotationEditor;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.ReferenceGenomeEditor;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.ReferenceSequenceEditor;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.TextInputEditor;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.TextSelectionEditor;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.tree.TreeSelectionEditor;
 import org.iplantc.de.apps.widgets.client.view.editors.style.AppTemplateWizardAppearance;
 
 /**
@@ -14,5 +31,21 @@ public interface ArgumentEditorGinFactory {
     FolderInputEditor folderInputEditor(AppTemplateWizardAppearance appearance);
     MultiFileInputEditor multiFileInputEditor(AppTemplateWizardAppearance appearance);
     FileFolderInputEditor fileFolderInputEditor(AppTemplateWizardAppearance appearance);
-
+    EnvironmentVariableEditor environmentVariableEditor(AppTemplateWizardAppearance appearance);
+    FlagEditor flagEditor(AppTemplateWizardAppearance appearance);
+    InfoEditor infoEditor(AppTemplateWizardAppearance appearance);
+    MultiLineTextEditor multiLineTextEditor(AppTemplateWizardAppearance appearance);
+    IntegerInputEditor integerInputEditor(AppTemplateWizardAppearance appearance);
+    DoubleInputEditor doubleInputEditor(AppTemplateWizardAppearance appearance);
+    TextInputEditor textInputEditor(AppTemplateWizardAppearance appearance);
+    TextSelectionEditor textSelectionEditor(AppTemplateWizardAppearance appearance);
+    IntegerSelectionEditor integerSelectionEditor(AppTemplateWizardAppearance appearance);
+    DoubleSelectionEditor doubleSelectionEditor(AppTemplateWizardAppearance appearance);
+    TreeSelectionEditor treeSelectionEditor(AppTemplateWizardAppearance appearance);
+    FileOutputEditor fileOutputEditor(AppTemplateWizardAppearance appearance);
+    FolderOutputEditor folderOutputEditor(AppTemplateWizardAppearance appearance);
+    MultiFileOutputEditor multiFileOutputEditor(AppTemplateWizardAppearance appearance);
+    ReferenceGenomeEditor referenceGenomeEditor(AppTemplateWizardAppearance appearance);
+    ReferenceSequenceEditor referenceSequenceEditor(AppTemplateWizardAppearance appearance);
+    ReferenceAnnotationEditor referenceAnnotationEditor(AppTemplateWizardAppearance appearance);
 }
