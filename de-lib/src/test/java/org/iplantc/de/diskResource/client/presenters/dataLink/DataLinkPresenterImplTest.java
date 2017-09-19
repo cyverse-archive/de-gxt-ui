3707f8644fccf850d10e33669ce59e9dfd270d0a
Ashley Ramsey <aramsey@iplantcollaborative.org>
2017-09-19 15:02:10 -0700

    CORE-8621 Update tests for DataLinkPresenter

--- de-lib/src/test/java/org/iplantc/de/diskResource/client/presenters/dataLink/DataLinkPresenterImplTest.java
+++ de-lib/src/test/java/org/iplantc/de/diskResource/client/presenters/dataLink/DataLinkPresenterImplTest.java
@@ -1,23 +1,25 @@
 package org.iplantc.de.diskResource.client.presenters.dataLink;
 
 import static org.mockito.Matchers.anyListOf;
-import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.iplantc.de.client.models.dataLink.DataLink;
-import org.iplantc.de.client.models.dataLink.DataLinkFactory;
+import org.iplantc.de.client.models.dataLink.UpdateTicketResponse;
 import org.iplantc.de.client.models.diskResources.DiskResource;
 import org.iplantc.de.client.services.DiskResourceServiceFacade;
 import org.iplantc.de.client.util.DiskResourceUtil;
 import org.iplantc.de.diskResource.client.DataLinkView;
+import org.iplantc.de.diskResource.client.events.selection.CreateDataLinkSelected;
 import org.iplantc.de.diskResource.client.events.selection.DeleteDataLinkSelected;
-import org.iplantc.de.diskResource.client.gin.factory.DataLinkViewFactory;
+import org.iplantc.de.diskResource.client.events.selection.ShowDataLinkSelected;
+import org.iplantc.de.diskResource.client.views.dialogs.DataLinkDialog;
+import org.iplantc.de.shared.AsyncProviderWrapper;
 import org.iplantc.de.shared.DECallback;
 
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwtmockito.GwtMockitoTestRunner;
 
 import com.sencha.gxt.core.shared.FastMap;
@@ -40,42 +42,42 @@ public class DataLinkPresenterImplTest {
 
     @Mock DataLinkView viewMock;
     @Mock DiskResourceServiceFacade drServiceMock;
-    @Mock DataLinkFactory dlFactoryMock;
     @Mock DiskResourceUtil diskResourceUtilMock;
     @Mock List<DiskResource> resourcesMock;
     @Mock List<DiskResource> diskResourceListMock;
-    @Mock DataLinkViewFactory dataLinkViewFactoryMock;
     @Mock Iterator<DiskResource> diskResourceIteratorMock;
     @Mock DiskResource resourceMock;
     @Mock List<String> stringListMock;
     @Mock DataLink dataLinkMock;
     @Mock DataLinkView.Appearance appearanceMock;
+    @Mock AsyncProviderWrapper<DataLinkDialog> dataLinkDlgProviderMock;
+    @Mock DataLinkDialog dataLinkDlgMock;
 
-    @Captor ArgumentCaptor<DECallback<String>> stringCallbackCaptor;
+    @Captor ArgumentCaptor<DECallback<UpdateTicketResponse>> ticketResponseCallbackCaptor;
     @Captor ArgumentCaptor<DECallback<List<DataLink>>> dataLinkCallbackCaptor;
-    @Captor
-    ArgumentCaptor<DECallback<FastMap<List<DataLink>>>> mapLinkCallbackCaptor;
+    @Captor ArgumentCaptor<DECallback<FastMap<List<DataLink>>>> mapLinkCallbackCaptor;
+    @Captor ArgumentCaptor<AsyncCallback<DataLinkDialog>> dataLinkDlgCaptor;
 
 
     private DataLinkPresenterImpl uut;
 
     @Before
     public void setUp() {
-        when(dataLinkViewFactoryMock.create(isA(DataLinkPresenterImpl.class), eq(resourcesMock))).thenReturn(viewMock);
         when(resourcesMock.size()).thenReturn(2);
         when(resourcesMock.iterator()).thenReturn(diskResourceIteratorMock);
         when(diskResourceIteratorMock.hasNext()).thenReturn(true, true, false);
         when(diskResourceIteratorMock.next()).thenReturn(resourceMock, resourceMock);
         when(appearanceMock.loadingMask()).thenReturn("loading...");
+        when(diskResourceUtilMock.asStringPathList(resourcesMock)).thenReturn(stringListMock);
 
         uut = new DataLinkPresenterImpl(drServiceMock,
-                                        dataLinkViewFactoryMock,
-                                        dlFactoryMock,
-                                        diskResourceUtilMock,appearanceMock,
+                                        viewMock,
+                                        diskResourceUtilMock,
+                                        appearanceMock,
                                         resourcesMock){
             @Override
             List<DiskResource> createDiskResourcesList() {
-                return diskResourceListMock;
+                return resourcesMock;
             }
 
             @Override
@@ -84,21 +86,21 @@ public class DataLinkPresenterImplTest {
             }
         };
 
+        uut.dataLinkDlgProvider = dataLinkDlgProviderMock;
+
         verifyConstructor();
     }
 
     private void verifyConstructor() {
-        verify(dataLinkViewFactoryMock).create(isA(DataLinkPresenterImpl.class), eq(resourcesMock));
-        verify(diskResourceListMock, times(2)).add(resourceMock);
+        verify(viewMock).addCreateDataLinkSelectedHandler(eq(uut));
+        verify(viewMock).addAdvancedSharingSelectedHandler(eq(uut));
+        verify(viewMock).addShowDataLinkSelectedHandler(eq(uut));
+        verify(viewMock).addDeleteDataLinkSelectedHandler(eq(uut));
+
+        testGetExistingDataLinks();
     }
 
-    @Test
-    public void testGetExistingDataLinks() {
-        when(diskResourceUtilMock.asStringPathList(resourcesMock)).thenReturn(stringListMock);
-
-        /** CALL METHOD UNDER TEST **/
-        uut.getExistingDataLinks(resourcesMock);
-
+    private void testGetExistingDataLinks() {
         verify(viewMock).addRoots(eq(resourcesMock));
         verify(drServiceMock).listDataLinks(eq(stringListMock), mapLinkCallbackCaptor.capture());
     }
@@ -111,17 +113,34 @@ public class DataLinkPresenterImplTest {
 
         /** CALL METHOD UNDER TEST **/
         uut.onDeleteDataLinkSelected(eventMock);
-        verify(drServiceMock).deleteDataLinks(anyListOf(String.class), stringCallbackCaptor.capture());
+        verify(drServiceMock).deleteDataLinks(anyListOf(String.class), ticketResponseCallbackCaptor.capture());
     }
 
     @Test
     public void testCreateDataLinks() {
+        CreateDataLinkSelected eventMock = mock(CreateDataLinkSelected.class);
+        when(eventMock.getSelectedDiskResources()).thenReturn(resourcesMock);
         when(dataLinkMock.getPath()).thenReturn("string");
 
         /** CALL METHOD UNDER TEST **/
-        uut.createDataLinks(resourcesMock);
+        uut.onCreateDataLinkSelected(eventMock);
 
         verify(viewMock).mask(appearanceMock.loadingMask());
         verify(drServiceMock).createDataLinks(eq(stringListMock), dataLinkCallbackCaptor.capture());
+    }
+
+    @Test
+    public void onShowDataLinkPresented() {
+        ShowDataLinkSelected eventMock = mock(ShowDataLinkSelected.class);
+        when(eventMock.getSelectedResource()).thenReturn(dataLinkMock);
+        when(dataLinkMock.getDownloadUrl()).thenReturn("url");
+
+        /** CALL METHOD UNDER TEST **/
+        uut.onShowDataLinkSelected(eventMock);
+
+        verify(dataLinkDlgProviderMock).get(dataLinkDlgCaptor.capture());
+
+        dataLinkDlgCaptor.getValue().onSuccess(dataLinkDlgMock);
+        verify(dataLinkDlgMock).show(eq("url"));
     }
 }
