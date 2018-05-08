16216f8acaa5f255f29ff07021425bbdd293b141
Sriram Srinivasan <sriram@cyverse.org>
2018-05-08 15:23:41 -0700

    A util to for displaying cyverse style hyperlink.

--- react-components/stories/index.stories.js
+++ react-components/stories/index.stories.js
@@ -1,20 +1,20 @@
-import React from 'react';
-
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-
-import ToolDetailsTest from './apps/details/ToolDetails.stories'
-import CategoryTreeTest from './apps/details/CategoryTree.stories'
-import CopyTextAreaTest from './util/CopyTextArea.stories'
-import BasicDetailsTest from './data/details/BasicDetails.stories'
-import InfoTypeSelectionListTest from './data/details/InfoTypeSelectionList.stories'
-import TagTest from './data/details/Tag.stories'
-import TagPanelTest from './data/details/TagPanel.stories'
-import AppStatsTest from './apps/admin/AppStats.stories'
+import React from "react";
+import {storiesOf} from "@storybook/react";
+import {action} from "@storybook/addon-actions";
+import ToolDetailsTest from "./apps/details/ToolDetails.stories";
+import CategoryTreeTest from "./apps/details/CategoryTree.stories";
+import CopyTextAreaTest from "./util/CopyTextArea.stories";
+import BasicDetailsTest from "./data/details/BasicDetails.stories";
+import InfoTypeSelectionListTest from "./data/details/InfoTypeSelectionList.stories";
+import TagTest from "./data/details/Tag.stories";
+import TagPanelTest from "./data/details/TagPanel.stories";
+import AppStatsTest from "./apps/admin/AppStats.stories";
+import DEHyperLinkTest from "./util/hyperlink/DEHyperLink.stories";
 
 storiesOf('apps/details', module).add('CategoryTree', () => <CategoryTreeTest logger={action('hierarchy')} />);
 storiesOf('apps/details', module).add('ToolDetails', () => <ToolDetailsTest/>);
 storiesOf('util', module).add('CopyTextArea', () => <CopyTextAreaTest/>);
+storiesOf('util', module).add('DEHyperLink', () => <DEHyperLinkTest/>);
 
 
 storiesOf('apps/admin/AppStats', module).add('with test stats', () => <AppStatsTest/>);
