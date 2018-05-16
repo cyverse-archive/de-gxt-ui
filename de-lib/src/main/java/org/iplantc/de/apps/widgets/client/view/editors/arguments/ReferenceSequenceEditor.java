0dfdcc3b3ae3e027d180f4ea5d60b118f7052d68
Ashley Ramsey <aramsey@iplantcollaborative.org>
2018-05-15 17:21:34 -0700

    CORE-9224 Update analysis reference genome dropdown with latest genomes

--- de-lib/src/main/java/org/iplantc/de/apps/widgets/client/view/editors/arguments/ReferenceSequenceEditor.java
+++ de-lib/src/main/java/org/iplantc/de/apps/widgets/client/view/editors/arguments/ReferenceSequenceEditor.java
@@ -5,6 +5,7 @@ import static com.sencha.gxt.cell.core.client.form.ComboBoxCell.TriggerAction.AL
 import org.iplantc.de.apps.shared.AppsModule;
 import org.iplantc.de.apps.widgets.client.models.ReferenceGenomeProperties;
 import org.iplantc.de.apps.widgets.client.view.editors.arguments.converters.ArgumentEditorConverter;
+import org.iplantc.de.apps.widgets.client.view.editors.arguments.converters.ReferenceGenomeEditorConverter;
 import org.iplantc.de.apps.widgets.client.view.editors.arguments.converters.SplittableToReferenceGenomeConverter;
 import org.iplantc.de.apps.widgets.client.view.editors.style.AppTemplateWizardAppearance;
 import org.iplantc.de.client.models.apps.refGenome.ReferenceGenome;
@@ -35,7 +36,7 @@ public class ReferenceSequenceEditor extends AbstractArgumentEditor implements H
         ClearComboBoxSelectionKeyDownHandler handler = new ClearComboBoxSelectionKeyDownHandler(comboBox);
         comboBox.addKeyDownHandler(handler);
 
-        editorAdapter = new ArgumentEditorConverter<ReferenceGenome>(comboBox, new SplittableToReferenceGenomeConverter());
+        editorAdapter = new ReferenceGenomeEditorConverter<>(comboBox, new SplittableToReferenceGenomeConverter());
 
         argumentLabel.setWidget(editorAdapter);
     }
