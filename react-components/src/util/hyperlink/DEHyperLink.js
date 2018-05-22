e86362d30724e48fd1a58a69b4fdc9600a1bf9f6
Sriram Srinivasan <sriram@cyverse.org>
2018-05-22 16:42:06 -0700

    Remove unused constructor.

--- react-components/src/util/hyperlink/DEHyperLink.js
+++ react-components/src/util/hyperlink/DEHyperLink.js
@@ -5,10 +5,6 @@ import {css} from 'aphrodite';
 
 class DEHyperLink extends Component {
 
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <a className={css(styles.normal)}>{this.props.text}</a>
