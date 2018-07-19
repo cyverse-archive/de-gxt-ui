b1c25af4bd7b6a0bf105cc962313fb0bf5cc7879
Ash <asherz@email.arizona.edu>
2018-07-18 18:32:15 -0700

    Switch TagPanel component to use Autocomplete

--- react-components/src/data/details/TagPanel.js
+++ react-components/src/data/details/TagPanel.js
@@ -7,40 +7,10 @@ import withI18N, {getMessage} from "../../util/I18NWrapper";
 import intlData from "../messages";
 import build from "../../util/DebugIDUtil";
 import ids from "../ids";
-import Select from "react-select";
 import exStyle from "../style";
 import injectSheet from "react-jss";
-import {withStyles} from "@material-ui/core/styles";
 import PropTypes from 'prop-types';
-
-const styles = theme => ({
-    // We had to use a lot of global selectors in order to style react-select.
-    // We are waiting on https://github.com/JedWatson/react-select/issues/1679
-    // to provide a much better implementation.
-    // Also, we had to reset the default style injected by the library.
-    '@global': {
-        '.Select-input input': {
-            cursor: 'default',
-            display: 'block',
-            fontFamily: 'inherit',
-            fontSize: 'inherit',
-            margin: 0,
-            outline: 0,
-            width: '100% !important',
-        },
-        '.Select-menu-outer': {
-            backgroundColor: theme.palette.background.paper,
-            boxShadow: theme.shadows[2],
-            width: '100%',
-            zIndex: 2,
-            maxHeight: 100,
-        },
-        '.Select-menu': {
-            maxHeight: 100,
-            overflowY: 'auto',
-        },
-    },
-});
+import Autocomplete from "../../util/Autocomplete";
 
 class TagPanel extends Component {
 
@@ -71,16 +41,16 @@ class TagPanel extends Component {
         let { placeholder } = this.props;
         return (
             <div id={build(this.props.baseID, ids.DETAILS_TAGS_PANEL)}>
-                <Select.Creatable labelKey="value"
-                                  valueKey="id"
-                                  multi={false}
-                                  options={this.props.dataSource}
-                                  onInputChange={this.handleInputChange}
-                                  onChange={this.handleTagSelect}
-                                  placeholder={placeholder}
-                                  promptTextCreator={(tagValue) => {
-                                      return getMessage("newTagPrompt", {values: {tag: tagValue}});
-                                  } }
+                <Autocomplete labelKey="value"
+                              valueKey="id"
+                              multi={false}
+                              options={this.props.dataSource}
+                              onInputChange={this.handleInputChange}
+                              onChange={this.handleTagSelect}
+                              placeholder={placeholder}
+                              promptTextCreator={(tagValue) => {
+                                  return getMessage("newTagPrompt", {values: {tag: tagValue}});
+                              } }
                 />
                 <div id={build(this.props.baseID, ids.DETAILS_TAGS)}
                      className={classes.tagPanelStyle}>
@@ -107,4 +77,4 @@ TagPanel.defaultProps = {
     placeholder: getMessage("searchTags")
 };
 
-export default injectSheet(exStyle)(withStyles(styles)(withI18N(TagPanel, intlData)));
+export default injectSheet(exStyle)(withI18N(TagPanel, intlData));
