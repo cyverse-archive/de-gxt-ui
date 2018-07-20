2df69f417c9f4621fb6c62b2dd79fb7fdef468ae
Ashley Ramsey <aramsey@iplantcollaborative.org>
2018-07-20 12:56:45 -0700

    CORE-9210 Switch ReactSearchForm to load up QueryBuilder

--- de-lib/src/main/java/org/iplantc/de/diskResource/client/views/search/ReactSearchForm.java
+++ de-lib/src/main/java/org/iplantc/de/diskResource/client/views/search/ReactSearchForm.java
@@ -10,16 +10,15 @@ import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
-@JsType(isNative = true, namespace = "CyVerseReactComponents.data.search", name = "SearchForm")
+@JsType(isNative = true, namespace = "CyVerseReactComponents.data.search", name = "QueryBuilder")
 public class ReactSearchForm {
 
-    @JsProperty(namespace = "CyVerseReactComponents.data.search", name = "SearchForm")
+    @JsProperty(namespace = "CyVerseReactComponents.data.search", name = "QueryBuilder")
     public static ReactClass<SearchFormProps> SearchForm;
 
     @JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
     public static class SearchFormProps extends BaseProps {
         public SearchView.Presenter presenter;
-        public Splittable dateIntervals;
         public Splittable suggestedTags;
         public String id;
         public Splittable initialValues;
