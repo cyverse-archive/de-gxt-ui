01f3d0fb5f71d5ddfb5c60799274f917bb4e13b6
Sriram Srinivasan <sriram@cyverse.org>
2018-08-01 16:26:18 -0700

    core-9215 remove unused code.

--- react-components/src/desktop/desktop.js
+++
@@ -1,7 +0,0 @@
-import DesktopView from "./view/DesktopView";
-import UserMenu from "./view/UserMenu";
-import Notifications from "./view/Notifications";
-import Help from "./view/Help";
-import Taskbar from "./view/Taskbar";
-
-export {DesktopView, UserMenu, Notifications, Help, Taskbar};
