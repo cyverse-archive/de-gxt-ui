121c6cb290cb4b51d64dc9651da086dce6802c1f
Sriram Srinivasan <sriram@cyverse.org>
2018-08-10 13:47:48 -0700

    core-9254 add npm tests

---
+++ react-components/src/__tests__/notification.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { getDefaultTheme, MuiThemeProvider } from "../lib";
+import NotificationViewTest from "../../stories/notifications/view/NotificationView.stories";
+import DenyJoinRequestDetailsDialogTest
+    from "../../stories/notifications/view/dialogs/DenyJoinRequestDetailsDialog.stories";
+import JoinTeamRequestDialogTest
+    from "../../stories/notifications/view/dialogs/JoinTeamRequestDialog.stories";
+import RequestHistoryDialogTest
+    from "../../stories/notifications/view/dialogs/RequestHistoryDialog.stories";
+
+it('renders NotificationViewYest without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<MuiThemeProvider
+        theme={getDefaultTheme()}><NotificationViewTest/></MuiThemeProvider>, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+it('renders DenyJoinRequestDetailsDialogTest without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<MuiThemeProvider
+        theme={getDefaultTheme()}><DenyJoinRequestDetailsDialogTest/></MuiThemeProvider>, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+it('renders JoinTeamRequestDialogTest without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<MuiThemeProvider
+        theme={getDefaultTheme()}><JoinTeamRequestDialogTest/></MuiThemeProvider>, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+it('renders RequestHistoryDialogTest without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<MuiThemeProvider
+        theme={getDefaultTheme()}><RequestHistoryDialogTest/></MuiThemeProvider>, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
