ad54a9ee630f6ffc8bfdd77395ecddf38c2df984
Ashley Ramsey <aramsey@iplantcollaborative.org>
2018-10-23 15:41:30 -0700

    UI-346 Add admin endpoint for listing apps in a community

--- de-lib/src/main/java/org/iplantc/de/admin/desktop/client/communities/service/AdminCommunityServiceFacade.java
+++ de-lib/src/main/java/org/iplantc/de/admin/desktop/client/communities/service/AdminCommunityServiceFacade.java
@@ -1,6 +1,8 @@
 package org.iplantc.de.admin.desktop.client.communities.service;
 
+import org.iplantc.de.client.models.apps.App;
 import org.iplantc.de.client.models.groups.Group;
+import org.iplantc.de.shared.DECallback;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
@@ -16,4 +18,11 @@ public interface AdminCommunityServiceFacade {
      * @param communityCallback
      */
     void getCommunities(AsyncCallback<List<Group>> communityCallback);
+
+    /**
+     * Fetch the list of apps that belong to a community
+     * @param community
+     * @param appListCallback
+     */
+    void getCommunityApps(Group community, DECallback<List<App>> appListCallback);
 }
--- de-lib/src/main/java/org/iplantc/de/admin/desktop/client/communities/service/impl/AdminCommunityServiceFacadeImpl.java
+++ de-lib/src/main/java/org/iplantc/de/admin/desktop/client/communities/service/impl/AdminCommunityServiceFacadeImpl.java
@@ -3,14 +3,21 @@ package org.iplantc.de.admin.desktop.client.communities.service.impl;
 import static org.iplantc.de.shared.services.BaseServiceCallWrapper.Type.GET;
 
 import org.iplantc.de.admin.desktop.client.communities.service.AdminCommunityServiceFacade;
+import org.iplantc.de.client.models.apps.App;
+import org.iplantc.de.client.models.apps.proxy.AppListLoadResult;
+import org.iplantc.de.client.models.apps.proxy.AppSearchAutoBeanFactory;
 import org.iplantc.de.client.models.groups.Group;
 import org.iplantc.de.client.models.groups.GroupAutoBeanFactory;
+import org.iplantc.de.client.services.converters.DECallbackConverter;
 import org.iplantc.de.client.services.converters.GroupListCallbackConverter;
+import org.iplantc.de.shared.DECallback;
 import org.iplantc.de.shared.services.DiscEnvApiService;
 import org.iplantc.de.shared.services.ServiceCallWrapper;
 
+import com.google.gwt.http.client.URL;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.inject.Inject;
+import com.google.web.bindery.autobean.shared.AutoBeanCodex;
 
 import java.util.List;
 
@@ -20,7 +27,9 @@ import java.util.List;
 public class AdminCommunityServiceFacadeImpl implements AdminCommunityServiceFacade {
 
     private final String ADMIN_COMMUNITIES = "org.iplantc.services.admin.communities";
+    private final String ADMIN_APPS_COMMUNITIES = "org.iplantc.services.admin.apps.communities";
     @Inject GroupAutoBeanFactory factory;
+    @Inject AppSearchAutoBeanFactory appAutoBeanFactory;
     @Inject private DiscEnvApiService deService;
 
     @Inject
@@ -35,4 +44,17 @@ public class AdminCommunityServiceFacadeImpl implements AdminCommunityServiceFac
         deService.getServiceData(wrapper, new GroupListCallbackConverter(factory, callback));
     }
 
+    @Override
+    public void getCommunityApps(Group community, DECallback<List<App>> appListCallback) {
+        String address = ADMIN_APPS_COMMUNITIES + "/" + URL.encode(community.getDisplayName()) + "/apps";
+
+        ServiceCallWrapper wrapper = new ServiceCallWrapper(GET, address);
+        deService.getServiceData(wrapper, new DECallbackConverter<String, List<App>>(appListCallback) {
+            @Override
+            protected List<App> convertFrom(String object) {
+                AppListLoadResult listResult = AutoBeanCodex.decode(appAutoBeanFactory, AppListLoadResult.class, object).as();
+                return listResult.getData();
+            }
+        });
+    }
 }
