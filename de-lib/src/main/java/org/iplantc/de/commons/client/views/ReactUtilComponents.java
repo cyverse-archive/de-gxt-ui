2b2654d92539011022aae7f498197c863186b255
Ash <asherz@email.arizona.edu>
2019-01-16 12:03:38 -0700

    Add react version of ErrorHandler in GWT

---
+++ de-lib/src/main/java/org/iplantc/de/commons/client/views/ReactUtilComponents.java
@@ -0,0 +1,22 @@
+package org.iplantc.de.commons.client.views;
+
+import gwt.react.client.components.ReactClass;
+import gwt.react.client.proptypes.BaseProps;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+
+/**
+ * @author aramsey
+ */
+@JsType(isNative = true, namespace = "CyVerseReactComponents", name = "util")
+public class ReactUtilComponents {
+    @JsProperty
+    public static ReactClass<ErrorHandlerProps> ErrorHandler;
+
+    @JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
+    public static class ErrorHandlerProps extends BaseProps {
+        public String errorSummary;
+        public String errorDetails;
+    }
+}
