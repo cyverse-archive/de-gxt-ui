ec2f09ac2025cd655b977b6d2db36111b85c7bdd
Sriram Srinivasan <sriram@cyverse.org>
2019-02-12 13:07:24 -0700

    Remove page size used for testing.

--- react-components/src/analysis/view/AnalysesView.js
+++ react-components/src/analysis/view/AnalysesView.js
@@ -973,7 +973,7 @@ class AnalysesView extends Component {
                                      onChangePage={this.handleChangePage}
                                      onChangeRowsPerPage={this.handleChangeRowsPerPage}
                                      ActionsComponent={TablePaginationActions}
-                                     rowsPerPageOptions={[5, 100, 500, 1000]}
+                                     rowsPerPageOptions={[100, 500, 1000]}
                     />
                 </div>
                 {selectedAnalysis &&
