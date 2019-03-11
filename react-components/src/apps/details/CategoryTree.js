5166a4c2741def12de78b7e483feab2e7fb50a60
Sriram Srinivasan <sriram@cyverse.org>
2019-03-11 16:35:59 -0700

    core-52 Use custom icon for hierarchies.

--- react-components/src/apps/details/CategoryTree.js
+++ react-components/src/apps/details/CategoryTree.js
@@ -4,47 +4,49 @@
 import React, { Component } from "react";
 import Tree, { TreeNode } from "rc-tree";
 import "rc-tree/assets/index.css";
+import Book from "../../resources/images/book.png";
+import Book_Open from "../../resources/images/book_open.png";
+import Book_Add from "../../resources/images/book_add.png";
 
+
+const Icon = (props) => {
+    const {expanded, isLeaf, title} = props;
+    if (expanded) {
+        return <img src={Book_Open} alt={title}/>
+    } else {
+        if (isLeaf) {
+            return <img src={Book} alt={title}/>;
+        } else {
+            return <img src={Book_Add} alt={title}/>;
+        }
+
+    }
+};
 class CategoryTree extends Component {
-    constructor(props) {
-        super(props);
-
-        // This binding is necessary to make `this` work in the callback
-        this.onDetailsCategoryClicked = this.onDetailsCategoryClicked.bind(
-            this
-        );
-    }
-
-    onDetailsCategoryClicked(selectedKeys, event) {
-        let modelKey = selectedKeys[0];
-        this.props.presenter.onDetailsCategoryClicked(modelKey);
-    }
-
     renderHierarchyNode(hierarchyClass, parentKey) {
         let modelKey =
             (parentKey ? parentKey + "/" : "") + hierarchyClass.label;
         return (
-            <TreeNode key={modelKey} title={hierarchyClass.label}>
-                {hierarchyClass.subclasses
-                    ? hierarchyClass.subclasses.map((subclass) =>
-                          this.renderHierarchyNode(subclass, modelKey)
-                      )
-                    : null}
+            <TreeNode key={modelKey}
+                      title={hierarchyClass.label}
+                      icon={Icon}
+                      isLeaf={hierarchyClass.subclasses ? false : true}>
+                {
+                    hierarchyClass.subclasses ?
+                        hierarchyClass.subclasses.map( (subclass) => this.renderHierarchyNode(subclass, modelKey) )
+                        : null
+                }
             </TreeNode>
         );
     }
-
     render() {
-        let hierarchies = this.props.app.hierarchies;
+        let hierarchies = this.props.hierarchies;
 
         return (
             <Tree
                 defaultExpandAll={false}
-                onSelect={this.onDetailsCategoryClicked}
-            >
-                {hierarchies.map((hierarchyClass) =>
-                    this.renderHierarchyNode(hierarchyClass)
-                )}
+                onExpand={this.onExpand}>
+                {hierarchies.map( (hierarchyClass) => this.renderHierarchyNode(hierarchyClass) )}
             </Tree>
         );
     }
