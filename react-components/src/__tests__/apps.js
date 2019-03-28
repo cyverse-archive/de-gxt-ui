594102f6ef99652f12b9d8cfa65dff80924f2249
Sriram Srinivasan <sriram@cyverse.org>
2019-03-28 12:12:37 -0700

    add npm test for app details

--- react-components/src/__tests__/apps.js
+++ react-components/src/__tests__/apps.js
@@ -1,20 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import { getDefaultTheme, MuiThemeProvider } from "../lib";
 
-import CategoryTreeTest from "../../stories/apps/details/CategoryTree.stories";
-import ToolDetailsTest from "../../stories/apps/details/ToolDetails.stories";
-import AppStatsTest from "../../stories/apps/admin/AppStats.stories";
+import CategoryTreeTest from '../../stories/apps/details/CategoryTree.stories';
+import ToolDetailsTest from '../../stories/apps/details/ToolDetails.stories';
+import AppStatsTest from '../../stories/apps/admin/AppStats.stories';
+import AppDetailsTest from "../../stories/apps/details/AppDetails.stories";
+import AppDocTest from "../../stories/apps/details/AppDoc.stories";
+import AppInfoDialogTest from "../../stories/apps/details/AppInfoDialog.stories";
 
-it("renders CategoryTree without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(
-        <MuiThemeProvider theme={getDefaultTheme()}>
-            <CategoryTreeTest />
-        </MuiThemeProvider>,
-        div
-    );
+it('renders CategoryTree without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<MuiThemeProvider theme={getDefaultTheme()}><CategoryTreeTest/></MuiThemeProvider>,
+        div);
     ReactDOM.unmountComponentAtNode(div);
 });
 
@@ -29,13 +28,32 @@ it("renders ToolDetails without crashing", () => {
     ReactDOM.unmountComponentAtNode(div);
 });
 
-it("renders ToolDetails without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(
-        <MuiThemeProvider theme={getDefaultTheme()}>
-            <AppStatsTest />
-        </MuiThemeProvider>,
-        div
-    );
+it('renders AppStats without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<MuiThemeProvider theme={getDefaultTheme()}><AppStatsTest/></MuiThemeProvider>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders AppDetails without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<MuiThemeProvider
+        theme={getDefaultTheme()}><AppDetailsTest/></MuiThemeProvider>, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+
+it('renders AppDoc without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<MuiThemeProvider theme={getDefaultTheme()}><AppDocTest/></MuiThemeProvider>, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+
+it('renders AppInfoDialog without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<MuiThemeProvider theme={getDefaultTheme()}><AppInfoDialogTest/></MuiThemeProvider>,
+        div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+
