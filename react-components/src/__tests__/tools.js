37dde4b497b3ae67fe5c32348353f211a58a053e
Sriram Srinivasan <sriram@cyverse.org>
2019-03-28 12:40:15 -0700

    add npm test for NewToolRequestForm

--- react-components/src/__tests__/tools.js
+++ react-components/src/__tests__/tools.js
@@ -4,12 +4,23 @@ import ReactDOM from "react-dom";
 import { getDefaultTheme, MuiThemeProvider } from "../lib";
 
 import EditToolTest from "../../stories/tools/EditTool.stories";
+import NewToolRequestFormTest from "../../stories/tools/requests/NewToolRequestsForm.stories";
 
 it("renders EditToolTest without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(
         <MuiThemeProvider theme={getDefaultTheme()}>
             <EditToolTest />
+        </MuiThemeProvider>,
+        div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+});
+it("renders NewToolRequestForm without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(
+        <MuiThemeProvider theme={getDefaultTheme()}>
+            <NewToolRequestFormTest />
         </MuiThemeProvider>,
         div
     );
