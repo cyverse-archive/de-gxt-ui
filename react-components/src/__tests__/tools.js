27de2422302664d8858cc3c77befaa71cd798c10
Sriram Srinivasan <sriram@cyverse.org>
2019-04-09 10:56:21 -0700

    run prettier

--- react-components/src/__tests__/tools.js
+++ react-components/src/__tests__/tools.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { getDefaultTheme, MuiThemeProvider } from "../lib";
-
 import EditToolTest from "../../stories/tools/EditTool.stories";
 import ManageToolsTest from "../../stories/tools/ManageTools.stories";
 import NewToolRequestFormTest from "../../stories/tools/requests/NewToolRequestsForm.stories";
