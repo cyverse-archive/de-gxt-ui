4f733b1e58d4ec74ed06a21a7b8219c5503a8d18
Jack Mittelmeier <jackmittelmeier@email.arizona.edu>
2019-05-09 12:00:47 -0700

    add retract comment function and formatting

--- react-components/stories/data/edit/EditComments.stories.js
+++ react-components/stories/data/edit/EditComments.stories.js
@@ -1,12 +1,16 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import EditComments from "./../../../src/data/edit/EditComments";
 
 class EditCommentsTest extends Component {
-    render(){
+    render() {
         const presenter = {
             getComments: (input, resolve, reject) => {
                 resolve(commentList);
-            }
+            },
+
+            retractComment: (commentId, resolve, reject) => {
+                console.log("Retracting comment with id: " + commentId);
+            },
         };
 
         const commentList = [
@@ -16,43 +20,40 @@ class EditCommentsTest extends Component {
                 id: "1",
                 date: "today",
                 retracted: true,
-
             },
             {
                 owner: "a scientist",
-                message: "I am writting this long comment because i am a scientist and that is what i do",
+                message:
+                    "I am writting this long comment because i am a scientist and that is what i do",
                 id: "2",
                 date: "04/1/2019",
-
             },
             {
                 owner: "donald j trump",
                 message: "thank you cyverse, very cool!",
                 id: "3",
-                date: "MM/DD/YYYY"
+                date: "MM/DD/YYYY",
             },
             {
                 owner: "retracted commentet",
                 message: "doesn't really matter",
-                id:"4",
-                date:"12/12/2012",
-                retracted: true
+                id: "4",
+                date: "12/12/2012",
+                retracted: true,
             },
             {
                 owner: "creator",
-                message: "i created this file, please comment and say things. javascript",
+                message:
+                    "i created this file, please comment and say things. javascript",
                 id: "5",
-                date:"the/dawn/oftime",
-
+                date: "the/dawn/oftime",
             },
         ];
 
         const fileName = "someFileName";
 
-        return <EditComments presenter={presenter}
-                             fileName={fileName}
-                            />
+        return <EditComments presenter={presenter} fileName={fileName} />;
     }
 }
 
-export default EditCommentsTest
+export default EditCommentsTest;
