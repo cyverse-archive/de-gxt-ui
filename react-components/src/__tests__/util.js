1d3adf7991fbcaf154c714396f8c26606af0828a
Sriram Srinivasan <sriram@cyverse.org>
2019-05-21 14:26:46 -0700

    remove unused test

--- react-components/src/__tests__/util.js
+++ react-components/src/__tests__/util.js
@@ -3,23 +3,11 @@ import ReactDOM from "react-dom";
 
 import { getDefaultTheme, MuiThemeProvider } from "../lib";
 
-import AutocompleteTest from "../../stories/util/Autocomplete.stories";
 import CopyTextAreaTest from "../../stories/util/CopyTextArea.stories";
 import ErrorExpansionPanel from "../../stories/util/ErrorExpansionPanel.stories";
 import ErrorHandlerTest from "../../stories/util/ErrorHandler.stories";
 import SearchFieldTest from "../../stories/util/SearchField.stories";
 import TriggerFieldTest from "../../stories/util/TriggerField.stories";
-
-it("renders Autocomplete without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(
-        <MuiThemeProvider theme={getDefaultTheme()}>
-            <AutocompleteTest />
-        </MuiThemeProvider>,
-        div
-    );
-    ReactDOM.unmountComponentAtNode(div);
-});
 
 it("renders CopyTextArea without crashing", () => {
     const div = document.createElement("div");
