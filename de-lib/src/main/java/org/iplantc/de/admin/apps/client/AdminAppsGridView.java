81a472d3d5b58ffa70c194912773d9b683c438e1
Ashley Ramsey <aramsey@iplantcollaborative.org>
2019-07-18 17:44:01 -0700

    CORE-329 Utilize AdminAppDetails in Belphegor

--- de-lib/src/main/java/org/iplantc/de/admin/apps/client/AdminAppsGridView.java
+++ de-lib/src/main/java/org/iplantc/de/admin/apps/client/AdminAppsGridView.java
@@ -14,14 +14,20 @@ import org.iplantc.de.apps.client.events.selection.AppSelectionChangedEvent;
 import org.iplantc.de.apps.client.events.selection.DeleteAppsSelected;
 import org.iplantc.de.client.models.IsMaskable;
 import org.iplantc.de.client.models.apps.App;
+import org.iplantc.de.client.services.callbacks.ReactErrorCallback;
+import org.iplantc.de.client.services.callbacks.ReactSuccessCallback;
 
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.IsWidget;
+import com.google.web.bindery.autobean.shared.Splittable;
 
 import com.sencha.gxt.data.shared.event.StoreRemoveEvent;
 import com.sencha.gxt.widget.core.client.grid.Grid;
 
 import java.util.List;
+
+import jsinterop.annotations.JsIgnore;
+import jsinterop.annotations.JsType;
 
 /**
  * Created by jstroot on 3/9/15.
@@ -43,6 +49,7 @@ public interface AdminAppsGridView extends IsWidget,
 
     }
 
+    @JsType
     interface Presenter extends AppCategorySelectionChangedEvent.AppCategorySelectionChangedEventHandler,
                                 DeleteAppsSelected.DeleteAppsSelectedHandler,
                                 StoreRemoveEvent.HasStoreRemoveHandler<App>,
@@ -81,11 +88,38 @@ public interface AdminAppsGridView extends IsWidget,
             String betaTagRemovedSuccess();
         }
 
+        @JsIgnore
         AdminAppsGridView getView();
 
+        @JsIgnore
         App getAppFromElement(Element eventTarget);
 
+        @JsIgnore
         List<App> getSelectedApps();
+
+        @SuppressWarnings("unusable-by-js")
+        void onSaveAppSelected(Splittable appSpl,
+                               ReactSuccessCallback callback,
+                               ReactErrorCallback errorCallback);
+
+        void addAppDocumentation(String systemId,
+                                 String appId,
+                                 String appDoc,
+                                 ReactSuccessCallback callback,
+                                 ReactErrorCallback errorCallback);
+
+        void updateAppDocumentation(String systemId,
+                                    String appId,
+                                    String appDoc,
+                                    ReactSuccessCallback callback,
+                                    ReactErrorCallback errorCallback);
+
+        @SuppressWarnings("unusable-by-js")
+        void updateBetaStatus(Splittable appSpl,
+                              ReactSuccessCallback callback,
+                              ReactErrorCallback errorCallback);
+
+        void closeAppDetailsDlg();
     }
 
     Grid<App> getGrid();
---
+++ de-lib/src/main/java/org/iplantc/de/admin/apps/client/ReactAppsAdmin.java
@@ -0,0 +1,41 @@
+package org.iplantc.de.admin.apps.client;
+
+import com.google.web.bindery.autobean.shared.Splittable;
+
+import gwt.react.client.components.ComponentConstructorFn;
+import gwt.react.client.proptypes.BaseProps;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+
+/**
+ * Created by sriram on 2/26/18.
+ */
+@JsType(isNative = true, namespace = "CyVerseReactComponents.apps", name = "admin")
+public class ReactAppsAdmin {
+
+    @JsProperty
+    public static ComponentConstructorFn<AppStatsProps> AppStats;
+
+    @JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
+    static class AppStatsProps extends BaseProps {
+        AdminAppStatsGridView.Presenter presenter;
+    }
+
+    @JsProperty
+    public static ComponentConstructorFn<AdminAppDetailsProps> AdminAppDetails;
+
+    @JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
+    public static class AdminAppDetailsProps extends BaseProps {
+        public AdminAppsGridView.Presenter presenter;
+        public Splittable app;
+        public String restrictedChars;
+        public String restrictedStartingChars;
+        public String createDocWikiUrl;
+        public String documentationTemplateUrl;
+        public String documentation;
+        public String parentId;
+        public boolean open;
+    }
+
+}
